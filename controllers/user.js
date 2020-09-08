a6597e88b8819261bcf502d883289100235e2a92
b_bado <bado.bernard94@gmail.com>
2020-09-08 12:38:08 +0200

    adding token refresh functionality

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
 const config = require("../config");
 
@@ -6,14 +7,37 @@ const config = require("../config");
  * Generate the token
  * @param {Object} user - user object
  */
-exports.generateToken = (user) => {
+exports.generateTokens = (user) => {
   const body = { id: user.id, email: user.email };
 
   // Sign the JWT token and populate the payload with the user email and id
-  const token = jwt.sign({ user: body }, config.accessTokenSecret, {
+  const accessToken = jwt.sign({ user: body }, config.accessTokenSecret, {
     expiresIn: config.accessTokenLife,
   });
 
+  // Sign the JWT refresh token
+  const refreshToken = jwt.sign({ user: body }, config.refreshTokenSecret, {
+    expiresIn: config.refreshTokenLife,
+  });
+
   // Send back the token to the user
-  return token;
+  return { accessToken, refreshToken };
+};
+
+exports.verifyRefreshToken = (refreshToken) => {
+  // Verify refresh token
+  return jwt.verify(
+    refreshToken,
+    config.refreshTokenSecret,
+    (error, { user }) => {
+      if (error) return false;
+
+      // Sign the JWT token and populate the payload with the user email and id
+      const accessToken = jwt.sign({ user }, config.accessTokenSecret, {
+        expiresIn: config.accessTokenLife,
+      });
+
+      return accessToken;
+    }
+  );
 };
--- routes/auth.js
+++ routes/auth.js
@@ -11,13 +11,30 @@ module.exports = (app) => {
     }
   );
 
+  app.get("/auth/refresh", (req, res) => {
+    // Extract token from header
+    const authHeader = req.header("authorization");
+    if (authHeader.startsWith("Bearer ")) {
+      const token = authHeader.substring(7, authHeader.length);
+
+      // Check if refresh token is valid. If yes, generate new access token
+      const accessToken = user.verifyRefreshToken(token);
+      if (!accessToken) {
+        return res.status(401).json({ error: "refresh_token_invalid" });
+      }
+      return res.json({ token: { accessToken } });
+    } else {
+      return res.status(401).json({ error: "refresh_token_invalid" });
+    }
+  });
+
   app.post(
     "/auth/signup",
     passport.authenticate("signup", { session: false }),
     (req, res) => {
       return res.json({
         user: req.user,
-        token: user.generateToken(req.user),
+        token: user.generateTokens(req.user),
       });
     }
   );
@@ -28,7 +45,7 @@ module.exports = (app) => {
     (req, res) => {
       return res.json({
         user: req.user,
-        token: user.generateToken(req.user),
+        token: user.generateTokens(req.user),
       });
     }
   );
